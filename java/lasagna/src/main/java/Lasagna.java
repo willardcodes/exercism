b2c501412be0d0fb83439490cf9b3c189d97bb01
William Raymond <william.c.raymond@protonmail.com>
2025-09-22 22:24:02 -0400

    Initial commit

---
+++ java/lasagna/src/main/java/Lasagna.java
@@ -0,0 +1,19 @@
+public class Lasagna {
+
+    public int expectedMinutesInOven() {
+        return 40;
+    }
+
+    public int remainingMinutesInOven(int actualMinutes) {
+        return this.expectedMinutesInOven() - actualMinutes;
+    }
+
+    public int preparationTimeInMinutes(int layers) {
+        return layers * 2;
+    }
+
+    public int totalTimeInMinutes(int layers, int actualMinutes) {
+        return preparationTimeInMinutes(layers) + actualMinutes;
+    }
+
+}
