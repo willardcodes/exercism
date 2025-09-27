74416c3cabb0aa8b4fae18a29e4af0e8b28309fa
William Raymond <william.c.raymond@protonmail.com>
2025-09-26 22:42:48 -0400

    Solve Need for Speed (#8)

---
+++ java/need-for-speed/src/main/java/NeedForSpeed.java
@@ -0,0 +1,47 @@
+class NeedForSpeed {
+
+    private int speed;
+    private int distance;
+    private int battery = 100;
+    private int batteryDrain;
+
+    NeedForSpeed(int speed, int batteryDrain) {
+        this.speed = speed;
+        this.batteryDrain = batteryDrain;
+    }
+
+    public boolean batteryDrained() {
+        return battery < batteryDrain;
+    }
+
+    public int distanceDriven() {
+        return this.distance;
+    }
+
+    public void drive() {
+        if (batteryDrain <= battery) {
+            this.distance += this.speed;
+            this.battery -= this.batteryDrain;
+        }
+    }
+
+    public static NeedForSpeed nitro() {
+        return new NeedForSpeed(50, 4);
+    }
+}
+
+class RaceTrack {
+
+    private final int distance;
+
+    RaceTrack(int distance) {
+        this.distance = distance;
+    }
+
+    public boolean canFinishRace(NeedForSpeed car) {
+        while (!car.batteryDrained()) {
+            car.drive();
+        }
+        return car.distanceDriven() >= this.distance;
+    }
+}
