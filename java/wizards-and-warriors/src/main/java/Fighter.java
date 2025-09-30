c8f8e87921fc85df75b9d5386a40a12659240d95
William Raymond <william.c.raymond@protonmail.com>
2025-09-29 21:07:47 -0400

    Solve Wizards and Warriors (#14)

---
+++ java/wizards-and-warriors/src/main/java/Fighter.java
@@ -0,0 +1,54 @@
+class Fighter {
+
+    boolean isVulnerable() {
+        return true;
+    }
+
+    int getDamagePoints(Fighter fighter) {
+        return 1;
+    }
+}
+
+class Warrior extends Fighter {
+
+    @Override
+    boolean isVulnerable() {
+        return false;
+    }
+
+    @Override
+    int getDamagePoints(Fighter fighter) {
+        return fighter.isVulnerable() ? 10 : 6;
+    }
+
+    @Override
+    public String toString() {
+        return "Fighter is a Warrior";
+    }
+
+}
+
+class Wizard extends Fighter {
+
+    private boolean spellPrepared = false;
+
+    void prepareSpell() {
+        this.spellPrepared = true;
+    }
+
+    @Override
+    boolean isVulnerable() {
+        return !this.spellPrepared;
+    }
+
+    @Override
+    int getDamagePoints(Fighter fighter) {
+        return spellPrepared ? 12 : 3;
+    }
+
+   @Override
+   public String toString() {
+       return "Fighter is a Wizard";
+   }
+
+}
